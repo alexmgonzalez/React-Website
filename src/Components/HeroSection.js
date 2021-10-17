19c89bd8966e7ea6dfd7259cd7edad267e415843
Alex Gonzalez <alexmgonzalez99@gmail.com>
2021-10-17 18:54:19 -0500

    changed file path

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src="./videos/video.mp4" autoPlay loop muted />
+            <video src="videos/video.mp4" autoPlay loop muted />
             <h1>HIRE ME PLEASE</h1>
             <p>You won't regret it!</p>
             <div className="hero-btns">
