9b7532da5e321c760a9673685031cc75240b637b
Alex Gonzalez <alexmgonzalez99@gmail.com>
2021-10-24 19:58:11 -0500

    hero section title

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
     return (
         <div className='hero-container'>
             <video src="videos/video.mp4" autoPlay loop muted/>
-            <h1>HIRE ME PLEASE</h1>
-            <p>You won't regret it!</p>
+            <h1>FOR HIRE</h1>
+            <p>Computer Science Graduate</p>
             <div className="hero-btns">
                 <Button 
                     className='btns' 
