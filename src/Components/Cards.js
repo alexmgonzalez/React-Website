aebc555de91e27a1834e9c21fa3f9479af9a6874
Alex Gonzalez <alexmgonzalez99@gmail.com>
2022-09-12 11:25:02 -0500

    changed picture for code card

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,23 +2,25 @@ import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
 import top from "./top";
+import image1 from "../images/resume2.jpg";
+import image2 from "../images/projects2.jpg";
 
 function Cards() {
     return (
         <div className="cards">
-            <h1>Check Me Out!</h1>
+            <h1>Take a Look!</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                            src="images/resume2.jpg"
+                            src={image1}
                             text="Take a look at my resume to see how I could fit on your team"
                             label="Resume"
                             path="/resume"
                             onClick={top}
                         />
                         <CardItem
-                            src="images/projects2.jpg"
+                            src={image2}
                             text="Check out some of my past Projects and see what I have done"
                             label="Projects"
                             path="/projects"
